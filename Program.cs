b2b080a14ce9b660a82d3606fa6eb438cc968ff3
Matt <mattmaliaros@gmail.com>
2019-11-12 22:48:15 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System;
+namespace DBConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Null connection String to test null values and timespan functionality
+            // String connect = null;
+
+            // OracleConnection OC = new OracleConnection(connect);
+            // OC.OpenConnection();
+            // OC.CloseConnection();
+            String sqlString = "SQL Connection";
+            SqlConnection sqlConnect = new SqlConnection(sqlString);
+            DBCommand newCommand = new DBCommand(sqlConnect);
+            newCommand.checkIfConnOpen();
+
+            // sqlConnect.OpenConnection();
+            // sqlConnect.CloseConnection();
+        }
+    }
+}
